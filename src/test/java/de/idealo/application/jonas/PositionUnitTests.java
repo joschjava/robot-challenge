0ff95e22d1c07c9da3890b24fa339051e624ae7e
Jonas Scholten <jonas.scholten@idealo.de>
2023-03-12 11:23:12 +0100

    Added more tests (whole route and unityFormat)

--- src/test/java/de/idealo/application/jonas/PositionUnitTests.java
+++ src/test/java/de/idealo/application/jonas/PositionUnitTests.java
@@ -90,4 +90,49 @@ class PositionUnitTests {
         assertEquals(expectedPos, pos);
     }
 
+    @Test
+    public void testToUnityFormat(){
+        Position pos = new Position(2, 3, Position.Rotation.NORTH);
+        String unityFormat = pos.toUnityFormat();
+        assertEquals("2;3;NORTH", unityFormat);
+    }
+
+    @Test
+    public void testRobotRoute1() {
+        Position robot = new Position(2, 2, Position.Rotation.EAST);
+
+        robot.forward(2); // 4, 2
+        robot.rotateRight(); // Rot SOUTH
+        robot.forward(2); // 4,4
+        robot.rotateRight(); //  ROT WEST
+        robot.forward(4); // 0,4
+        robot.waitNow();
+        robot.rotateRight(); // Rot NORTH
+        robot.forward(4); // 0, 0
+        robot.turnAround();  // Rot SOUTH
+
+        assertEquals(0, robot.getX());
+        assertEquals(0, robot.getY());
+        assertEquals(Position.Rotation.SOUTH, robot.getRotation());
+    }
+
+    @Test
+    public void testRobotRoute2() {
+        Position robot = new Position(0, 0, Position.Rotation.NORTH);
+
+        robot.rotateRight(); // (0, 0, EAST)
+        robot.forward(2); // (2, 0, EAST)
+        robot.waitNow(); // (2, 0, EAST)
+        robot.rotateRight(); // (2, 0, SOUTH)
+        robot.forward(3); // (2, 3, SOUTH)
+        robot.waitNow(); // (2, 3, SOUTH)
+        robot.turnAround(); // (2, 3, NORTH)
+        robot.forward(1); // (2, 2, NORTH)
+        robot.waitNow(); // (2, 2, NORTH)
+
+        assertEquals(2, robot.getX());
+        assertEquals(2, robot.getY());
+        assertEquals(Position.Rotation.NORTH, robot.getRotation());
+    }
+
 }
